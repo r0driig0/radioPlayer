cf60fdd4d12f033a7721542973f55cc53a386388
r0driig0 <rodrigorassosa@gmail.com>
2022-01-12 20:22:10 -0300

    timer delete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { setInterval} from 'timers';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-setInterval(1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
