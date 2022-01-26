c0e813a703f6505f6a519c8c2704310c61fdb102
r0driig0 <rodrigorassosa@gmail.com>
2022-01-25 21:07:09 -0300

    icon shop

--- src/component/redesSociales.js
+++ src/component/redesSociales.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { AiFillFacebook } from 'react-icons/ai';
-import { BsInstagram, BsSpotify, BsPaypal } from 'react-icons/bs';
+import { AiFillFacebook, AiFillShop } from 'react-icons/ai';
+import { BsInstagram, BsSpotify, BsPaypal,  } from 'react-icons/bs';
 import styles from "./redesSociales.module.css"
 
 
@@ -10,6 +10,7 @@ const redesSociales = () => {
     const instagramPage = "https://www.instagram.com/radiorockandmore/";
     const spotifyPage = 'https://open.spotify.com/user/hvnl5md0ge4dp86abw4ko89by?si=f44d53cbf7444def';
     const paypalPage = "https://www.paypal.com/donate/?hosted_button_id=8C7HE7P37LW24";
+    const redbubblePage = "https://www.redbubble.com/es/i/camiseta/Rock-And-More-Collection-de-rockandmoreok/100121295.IJ6L0";
 
     return (
         <div>
@@ -19,6 +20,7 @@ const redesSociales = () => {
                     <i onClick={() => window.open(instagramPage, '_blank')}><BsInstagram size={50} color={"#E1306C"} /></i>
                     <i onClick={() => window.open(spotifyPage, '_blank')}><BsSpotify size={50} color={"#81b71a"}/></i>
                     <i onClick={() => window.open(paypalPage, '_blank')}><BsPaypal size={50} color={"#0079C1"}/></i>
+                    <i onClick={() => window.open(redbubblePage, '_blank')}><AiFillShop size={50} color={"#DC143C"}/></i>
                 </div>
                 
             </div>
